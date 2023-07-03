3bc7922f286d208419e6be8a9559f9decd19ce5c
Mental007302 <sharjeelafzal444@gmail.com>
2023-07-03 02:08:07 -0700

    hello

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function login() {
+  return (
+    <>
+      <div className=' flex items-center justify-center w-screen h-screen'>
+        <div className='h-3/5 w-3/6 border border-gray-200 flex login-shadow'>
+          <div className="left w-1/2">
+            <div className="imgage h-full w-full"
+              style={{
+                backgroundImage: `url("/images/profile.jpg")`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+              }}>
+              <div className='w-full h-full bg-gray-800 bg-opacity-20'></div>
+            </div>
+          </div>
+
+          <div className="right w-1/2">
+            <div className='flex flex-col items-center justify-center h-full w-full'>
+              <h2 className="logo font-medium text-4xl text-gray-700">OutPost</h2>
+              <h2 className='text-2xl font-medium text-gray-700 pt-3 pb-6'>Login</h2>
+              <form action="" className='space-y-4 w-4/5 px-3'>
+                <div className='space-y-6'>
+                  <div className='flex flex-col space-y-1'>
+                    <label htmlFor="" className='font-medium text-gray-500'>Email</label>
+                    <div className='w-full border-b'>
+                      <input type="email" className='outline-none w-[250px] pb-2 text-sm' placeholder='Enter email address' />
+                    </div>
+                  </div>
+                  <div className='flex flex-col space-y-1'>
+                    <label htmlFor="" className='font-medium text-gray-500'>Password</label>
+                    <div className='w-full border-b'>
+                      <input type="password" className='outline-none w-[250px] pb-2 text-sm' placeholder='Password' />
+                    </div>
+                  </div>
+                </div>
+                <div className="button w-full pt-1">
+                  <button className='bg-[#0084ff] hover:bg-[#007aec] transition-all ease-in-out  text-white w-full py-3 rounded-md text-sm font-medium tracking-wide'>Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Edit, Apps, BookmarkBorder, AccountBoxOutlined } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
+export default function Profile() {
+     return (
+          <>
+               <div className='pt-14 mx-72'>
+                    <div className='flex flex-col relative justify-center items-center '>
+                         <div className='cover relative border rounded-b-lg  h-[430px] w-full border-gray-400'>
+                              <div className="profile absolute mx-auto right-0 left-0 -bottom-28 z-30 h-44 w-44 -mt-12 border border-black rounded-full">
+
+                              </div>
+                         </div>
+                    </div>
+                    <div className='flex  justify-between pt-10 mx-16 pb-10  border-b border-gray-300'>
+                         <div className="left">
+                              <div className='flex gap-5'>
+                                   <h2 className='font-medium text-xl tracking-wide'>UserName</h2>
+                                   <Link>
+                                        <div className='px-4 py-[6px] flex items-center gap-1 bg-gray-200 font-medium text-sm rounded-lg hover:bg-gray-300'>
+                                             <Edit style={{ fontSize: 16 }} />
+                                             Edit profile
+                                        </div>
+                                   </Link>
+                              </div>
+
+                              <div className='flex gap-3 pt-3'>
+                                   <span className='font-medium'>58</span>
+                                   <h3 className='font-normal'>followers</h3>
+                              </div>
+                              <div className='flex gap-3 '>
+                                   <span className='font-medium'>58</span>
+                                   <h3 className='font-normal'>following</h3>
+                              </div>
+                         </div>
+                         <div className="right w-1/3 pl-8">
+                              <div>
+                                   <h3 className='font-medium text-lg pb-1'>Intro</h3>
+                                   <h4 className='text-sm'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, saepe.</h4>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div className='buttons flex justify-center gap-8'>
+                         <div className="posts">
+                              <div className=' pt-3 flex items-center gap-1 text-gray-500 font-medium text-sm border-t border-white  hover:border-t hover:border-t-black hover:text-black hover:cursor-pointer'>
+                                   <Apps style={{ fontSize: 16 }} />
+                                   POSTS
+                              </div>
+                         </div>
+                         <div className="posts">
+                              <div className=' pt-3 flex items-center gap-1 text-gray-500 font-medium text-sm border-t border-white  hover:border-t hover:border-t-black hover:text-black hover:cursor-pointer'>
+                                   <BookmarkBorder style={{ fontSize: 16 }} />
+                                   SAVED
+                              </div>
+                         </div>
+                         <div className="posts">
+                              <div className=' pt-3 flex items-center gap-1 text-gray-500 font-medium text-sm border-t border-white  hover:border-t hover:border-t-black hover:text-black hover:cursor-pointer'>
+                                   <AccountBoxOutlined style={{ fontSize: 16 }} />
+                                   POSTS
+                              </div>
+                         </div>
+                         
+                    </div>
+               </div>
+          </>
+     )
+}
